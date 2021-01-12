3e4e4b6d794bb130560c41f8e20a259db0a3870a
Subham Karmakar <rijukarmakar24@gmail.com>
2021-01-12 17:02:38 +0530

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
+import React, { Component } from 'react';
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
 import firebaseConfig from './firebaseConfig';
 import logo from './logo.svg';
 import './App.css';
+
+firebase.initializeApp(firebaseConfig);
 
 function App()
 {
@@ -11,17 +14,7 @@ function App()
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                
             </header>
         </div>
     );
