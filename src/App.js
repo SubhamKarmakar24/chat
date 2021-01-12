d0d7294305e7babad84c56314fd0f93e394638e0
Subham Karmakar <rijukarmakar24@gmail.com>
2021-01-12 23:20:45 +0530

    Setup auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
@@ -8,13 +8,50 @@ import './App.css';
 
 firebase.initializeApp(firebaseConfig);
 
+
+function LoginState()
+{
+    const [user, setUser] = useState();
+
+    firebase.auth().onAuthStateChanged((user) =>
+    {
+        if(user)
+        {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            var uid = user.uid;
+            // ...
+            console.log("Logged in");
+            setUser(user);
+        }
+        else
+        {
+            // User is signed out
+            // ...
+            console.log("Logged out");
+        }
+    });
+    
+    if(user)
+    {
+        return <div>HI</div>;
+    }
+    else
+    {
+        return <div>BYE</div>;
+    }
+
+}
+
+
+
 function App()
 {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                
+                <LoginState />
             </header>
         </div>
     );
